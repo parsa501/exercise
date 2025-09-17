bd1449a0b6e4e2f577691bd4a8618b1b457d6e2e
parsa501 <safazadehparsa@gmail.com>
2025-09-17 17:04:23 -0500

    add user models

---
+++ Models/UserMd.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
